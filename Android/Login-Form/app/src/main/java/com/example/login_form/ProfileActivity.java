acb632805d730283241ac36ddb77f04b4c9ce00b
b‿daarr <118155606+b-daarr@users.noreply.github.com>
2024-12-23 15:20:55 +0330

    .

---
+++ Android/Login-Form/app/src/main/java/com/example/login_form/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.login_form;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
